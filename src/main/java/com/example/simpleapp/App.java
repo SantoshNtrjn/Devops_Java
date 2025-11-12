2aa765b93dc964189cc46459d624548c3a5d6b20
SantoshNtrjn <santoshpn03@gmail.com>
2025-11-12 08:39:19 +0000

    Update App.java

--- src/main/java/com/example/simpleapp/App.java
+++ src/main/java/com/example/simpleapp/App.java
@@ -3,5 +3,6 @@ package com.example.simpleapp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World! This is a simple Java application.");
+	System.out.println("This is a second line");
     }
 }
